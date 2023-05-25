ae576c3cf75bb1bdfaa8ac17019089f1669a1629
Ba Son <bagiangson2001@gmail.com>
2023-05-25 10:57:09 +0700

    up code

--- modules/model/bert.py
+++ modules/model/bert.py
@@ -13,14 +13,9 @@ class BertForTokenClassification(BertPreTrainedModel):
 
         self.init_weights()
 
-    def forward(self, input_ids, attention_mask=None, token_type_ids=None,
-                position_ids=None, head_mask=None, labels=None):
+    def forward(self, input_ids, attention_mask=None, labels=None):
 
-        outputs = self.bert(input_ids,
-                            attention_mask=attention_mask,
-                            token_type_ids=token_type_ids,
-                            position_ids=position_ids, 
-                            head_mask=head_mask)
+        outputs = self.bert(input_ids,attention_mask=attention_mask)
 
         sequence_output = outputs[0]
 
